a0f770381b1b91f012f86e1939dabc85ff39e0ca
mustafa0409 <100769598+mustafa0409@users.noreply.github.com>
2022-11-25 12:15:43 +0300

    Create ders2-odev.php

---
+++ ders2-odev.php
@@ -0,0 +1,25 @@
+
+<?php
+sınıf  Hesaplayıcı 
+   özel  $ num1 , $ num2 , $ num3 ;
+   genel  işlev  __construct ( $ sayı1 , $ sayı2 , $ sayı3 )
+   {
+    $ numara1 = $ numara1 ;
+    $ sayi2 = $ sayi2 ;
+    $ sayı3 = $ sayı3 ;
+   }
+    genel  işlev  TakeSquare ( $ inputnum ){
+     $ girdinum * $ girdinum'u döndürün ;
+   }
+
+    genel  işlev  Toplamı ( $ inputnum ){
+    için ( $ i = 0 ; $ i < $ girdi sayısı ; $ i ++) {
+        yankı  $ i * $ i ;
+        yankı " <br> ";
+    }
+    }
+}
+
+$ sınıfım = yeni  Hesaplayıcı ( 1 , 2 , 3 );
+$ sınıfım -> Toplam ( 8 );
+?>
