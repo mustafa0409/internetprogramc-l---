69ba64964b28ab40b4b52011a0a78e0513c24836
mustafa0409 <100769598+mustafa0409@users.noreply.github.com>
2022-11-25 12:18:54 +0300

    Create ders5-odev.php

---
+++ ders5-odev.php
@@ -0,0 +1,41 @@
+<?php
+yankı " <br> ";
+ soyut  sınıf  Sınıf2 {
+
+}
+
+ class  Sınıf1 , Sınıf2'yi genişletir  Arayuz1 , Arayuz2 , Arayuz3'ü uygular  { _
+
+    genel  işlev  __construct ( $ deg1 , $ deg2 , $ deg3 )
+    {
+        echo " Karşılama mesajı alındı. ";
+        yankı " <br> ";
+    }
+
+    genel  işlev  Met1 ( string  $ met1nesne ){
+        yankı " Adım " . $ met1nesne ;
+        yankı " <br> ";
+        
+    }
+
+    genel  işlev  Met2 ( string  $ met2nesne ){
+        yankı " Soyadım ". $ met2nesne ;
+    }
+}
+
+ $ nesne1 = new  S ınıf1( 1 , 2 , 3 );
+ $ nesne2 = new  S ınıf1( 1 , 2 , 3 );
+ $ nesne1 -> Met1 (" Furkan ");
+ $ nesne2 -> Met2 (" Güler ");
+
+ arayüz  Arayuz1 {
+
+}
+ arayüz  Arayuz2 {
+
+}
+arayüz  Arayuz3 {
+}
+
+
+?>
